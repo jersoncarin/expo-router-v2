d04cf7458f57c5f06845bff332a0bf7cb7bb296f
Jerson Carin <jersoncarin25@gmail.com>
2023-09-08 19:12:51 +0800

    Initial

---
+++ src/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router'
+
+export default function RootLayout() {
+  return <Text>hello world</Text>
+}
